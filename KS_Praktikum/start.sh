1ef4a37f15b32b55f1ff5a2e6a6f137407562d05
rAey <rfritz@gmx.de>
2013-01-07 15:46:14 +0100

    Update KS_Praktikum/start.sh

--- KS_Praktikum/start.sh
+++ KS_Praktikum/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+sudo iptables -A INPUT -p tcp --dport 33333 -j DROP
 # Starten des Beispielprogramms
-groovy -cp "src:libs/jpcap.jar" src/praktikum/beispiele/beispiel1/HttpBeispiel.groovy  -p "src/praktikum/beispiele/" $*
+sudo groovy -cp "src:libs/jpcap.jar" src/praktikum/beispiele/beispiel1/HttpBeispiel.groovy  -p "src/praktikum/beispiele/" $*
