e368f74b6d0a07b2533a0c5bc34376c957aed8b5
Sebastian Fischer <fischer.sebastian@gmail.com>
2013-01-19 21:45:07 +0100

    completed ARP_Beispiel.groovy and added threading to ARP.groovy
    ARP_Beispiel.groovy now instantiantes an ARP object, waits three seconds,
    and then uses it to send an ARP request to the IP specified with the -t
    command line parameter. Example usage:
    
      ARP_Beispiel.groovy -t 192.168.1.1
    
    The instantiated ARP object will print relevant information to STDOUT
    (ARP requests and replies received) and will answer ARP requests sent
    for our IP automatically.
    
    If ARP_Beispiel.groovy is invoked with the command line parameter -d
    it will print (a lot of) debugging information concerning the packets
    received from JPCAP.
    
    NOTE: I did not test the static methods of the ARP class extensively.
          My ARP request received a correct reply and ARP requests to the
          computer where the program was running were also correctly
          answered (twice, once by the OS and once by the program), so
          I assume they are working.

--- KS_Praktikum/src/praktikum/beispiele/arpBeispiel/ARP.groovy
+++ KS_Praktikum/src/praktikum/beispiele/arpBeispiel/ARP.groovy
@@ -1,37 +1,161 @@
 package praktikum.beispiele.arpBeispiel
+import praktikum.beispiele.utils.Utils
 import jpcap.*
 import jpcap.packet.*
-
-
+import java.util.concurrent.LinkedBlockingQueue
 
 /**
  * User: root
  * Date: 09.01.13
  * Time: 18:24
  */
-public class ARP {
+public class ARP implements PacketReceiver{
 
     //Jpcap devices
-    JpcapCaptor captor
-    JpcapSender sender
+    private JpcapCaptor captor
+    private JpcapSender sender
+
+    //threads
+    Thread receiverThread
+    Thread queueThread
+
+    boolean stopThreads = false
+    
+    boolean debug = false
+
+    // the network interface that jpcap will listen on
+    // passed to the constructor
+    String iface
+
+    String ownMacAddress
+    String ownIPAddress
+
+    InetAddress ownInetAddress
+
+    /** Broadcast MAC-Adresse */
+    final String broadcastMacAddress = "ff:ff:ff:ff:ff:ff"
+
+    LinkedBlockingQueue<Packet> recvQueue = new LinkedBlockingQueue()
 
     /*
     Constructor
      */
-    public Arp(){
-        captor = JpcapCaptor.openDevice(device,2000,false,3000);
+    ARP(String iface, String ownMacAddress, String ownIPAddress, boolean debug = false){
+       this.iface = iface
+        this.ownMacAddress = ownMacAddress
+        this.debug = debug
+        this.ownIPAddress = ownIPAddress
+        // binary representation
+        ownInetAddress = InetAddress.getByAddress(Utils.ipToByteArray(ownIPAddress))
+    }
+
+    /**
+     * starts threads
+     */
+    public void start() {
+        // when the program receives a SIGTEM (for example from a ctrl-c
+        // in the command line), it will finish it's threads cleanly
+        Runtime.addShutdownHook {stop()}
+
+        captor = JpcapCaptor.openDevice(Utils.getDevice(iface),65535,false,20);
         captor.setFilter("arp",true);
         sender=captor.getJpcapSenderInstance();
+        // starting the receiver thread, passing *this* as packet receiver
+        // this means we have to implement public void receivePacket(Packet p)
+        receiverThread = Thread.start { captor.loopPacket(-1, this) }
+        queueThread = Thread.start { processQueues() }
+        debug("[*] started jpcap receiver thread for Mac Address $ownMacAddress")
+    }
+
+    /**
+     * stops threads
+     */
+    public void stop() {
+        // this tells our processQueues thread to stop
+        stopThreads = true
+        // this tells the jpcap receiver thread to stop
+        captor.breakLoop()
+        debug("\n[*] waiting for threads to stop (1000ms)...")
+        sleep(1000)
+
+        // kill threads that are still alive
+        if (receiverThread != null && receiverThread.alive) receiverThread.destroy()
+        if (queueThread != null && queueThread.alive) queueThread.destroy()
+        debug("[*] killed remaining threads.")
+    }
+    
+    public void debug(String message) {
+        if (debug) {
+            println(message)
+        }
+    }
+
+    public void receivePacket(Packet p) {
+        debug("[R] received a packet from jpcap: " + p.toString())
+
+        EthernetPacket recvFrame = p.datalink as EthernetPacket
+        // Vergleich der Ziel-MAC-Adresse
+        if (!(recvFrame.destinationAddress == ownMacAddress ||
+                recvFrame.destinationAddress == broadcastMacAddress)) {
+            // Paket nicht an uns und nicht an Broadcast addressiert
+            debug("    [!] discarding: ethernet address mismatch")
+            return
+        }
+        if (recvFrame.frametype != EthernetPacket.ETHERTYPE_ARP) {
+            // kein ARP-Paket
+            debug("    [!] discarding: not an arp packet")
+            return
+        }
+
+        debug("    [*] matches our criteria => putting it in the receive queue")
+        recvQueue.put(p as ARPPacket)
+        debug("    [.] queue length is now " + recvQueue.size())
+    }
+
+    private void processQueues() {
+        debug("[*] queue thread has started.")
+        while(!stopThreads) {
+            if (!recvQueue.isEmpty()) {
+                debug("[*] processing an item in the receive queue")
+                processARPPacket(recvQueue.take() as ARPPacket)
+            }
+            sleep(40)
+        }
+    }
+
+    private void processARPPacket(ARPPacket p) {
+        // at this point we already know that this packet is addressed either
+        // to us or to broadcast, so it concerns us.
+
+        InetAddress target_protoaddr = InetAddress.getByAddress(p.target_protoaddr)
+        debug("[.] target_hardaddr is $p.target_hardaddr")
+        debug("[.] target_protoaddr is $target_protoaddr")
+        debug("[.] ownInetAddress is $ownInetAddress")
+        if (p.operation == ARPPacket.ARP_REQUEST &&
+            p.targetProtocolAddress == ownInetAddress) {
+            // this is an ARP request, and it requests our MAC
+            // we will send a reply
+            println("[*] ARP request from $p.senderProtocolAddress ($p.senderHardwareAddress)")
+            reply(p.senderProtocolAddress, p.sender_hardaddr, sender)
+            return
+        }
+
+        if (p.operation == ARPPacket.ARP_REPLY &&
+            p.targetHardwareAddress == ownMacAddress) {
+            // this is an ARP reply, and it is for us
+            println("[*] ARP reply from $p.senderProtocolAddress ($p.senderHardwareAddress)")
+        }
     }
 
 
+    // meines Erachtens ist diese Methode obsolet (wird durch ARP.request ersetzt)
     /**
      * requests the mac-adress from the connected lan-device
      * @param ip IP for which the MAC is to be requested
      * @return the MAC-Adress to the given ip
      * @throws IOException
      */
-    public static byte[] arpRequest(InetAddress ip) throws java.io.IOException{
+    public byte[] arpRequest(InetAddress ip) throws java.io.IOException{
         //find network interface
         NetworkInterface[] devices=JpcapCaptor.getDeviceList();
         NetworkInterface device=null;
@@ -145,6 +269,10 @@ loop:	for(NetworkInterface d:devices){
         return null;
     }
 
+    void request(String ip) {
+        request(InetAddress.getByAddress(Utils.ipToByteArray(ip)), sender)
+    }
+
     /**
      * Sendet einen ARP-Request an die angegebene IP Adresse.
      * @param ip Die IP zu der die MAC Adresse gesucht wird.
@@ -181,6 +309,10 @@ loop:	for(NetworkInterface d:devices){
         arp.datalink = ether;
 
         //Sendet das Packet über die angegebene Jpcap Instanz.
+        // TODO: laut ethercap geht das Paket korrekt raus, wir empfangen aber keine Antwort
+        println("[*] sending ARP request packet to $ip")
+        println("    sender_hardaddr = $arp.senderHardwareAddress")
+        println("    sender_protoaddr = $arp.senderProtocolAddress")
         sender.sendPacket(arp);
     }
 
--- KS_Praktikum/src/praktikum/beispiele/arpBeispiel/ARP_Beispiel.groovy
+++ KS_Praktikum/src/praktikum/beispiele/arpBeispiel/ARP_Beispiel.groovy
@@ -1,12 +1,96 @@
+#!/usr/bin/env groovy
+// Kopiert von praktikum.beispiele.beispiel1.httpBeispiel
+
 package praktikum.beispiele.arpBeispiel
 
-/**
- * Created with IntelliJ IDEA.
- * User: daniel
- * Date: 14.01.13
- * Time: 19:59
- * To change this template use File | Settings | File Templates.
- */
-class ARP_Beispiel {
+class arpBeispiel {
 
+    //========================================================================================================//
+    // Konfiguration der Ablaufumgebung ANFANG
+    //========================================================================================================//
+
+    /** Name der Konfigurationsdatei für die Ablaufumgebung */
+    static final String confFile = "env.conf"
+
+    /** Hier muß eine der Ablaufumgebungen eingestellt werden, die in der Konfigurationsdatei "env.conf"
+     * definiert sind (z.B. "home" oder "uni").
+     * Der Eintrag wird durch den optionalen Kommandozeilenparameter "e" überschrieben.
+     * Beim Start mit start.sh <br/>
+     * . start.sh [-e environment]<br/>
+     * oder als jar-file:<br/>
+     * unzip KS_Praktikum.jar; . startbin.sh [-e environment]
+     */
+    static String environment = "wlan"
+
+    /** Pfad zur Konfigurationsdatei für die Ablaufumgebung <br/>
+     * Der Eintrag wird durch den Kommandozeilenparameter "p" überschrieben.<br/>
+     * */
+    static String confFileName = "KS_Praktikum/src/praktikum/beispiele/"
+
+    //========================================================================================================//
+    // Konfiguration der Ablaufumgebung ENDE
+    //========================================================================================================//
+
+    static void main(String[] args)  {
+        boolean debug = false
+
+        // Behandlung von Kommandozeilenoptionen
+        def cli = new CliBuilder(usage: 'java | groovy ... arpBeispiel:')
+        cli.h(required: false, 'Hilfe anzeigen')
+        cli.e(required: false, args: 1, 'Name der zu verwendenden Ablaufumgebung')
+        cli.p(required: false, args: 1, "Pfad zu \'env.conf\', also z.B. \'praktikum/beispiele/\', nur beim Start von der Kommandozeile")
+        cli.d(required: false, args: 1, "Debug-Ausgaben aktivieren")
+        cli.t(required: true, args: 1, "IP-Adresse, der ein ARP-Request geschickt werden soll")
+
+        // Kommandozeilenparameter parsen
+        def options = cli.parse(args)
+
+        // Hilfe anzeigen
+        if (options.h) {
+            cli.usage()
+            return
+        }
+
+        if (options.d) {
+            debug = true
+        }
+
+        // Ablaufumgebung setzen
+        if (options.e) {
+            environment = options.e
+        }
+
+        // Pfad zur Konfigurationsdatei der Ablaufumgebungen setzen
+        confFileName += confFile
+        if (options.p) {
+            confFileName = options.p + confFile
+        }
+
+        File confFile = new File(confFileName)
+        ConfigObject config = new ConfigSlurper(environment).parse(confFile.toURL())
+
+        // ------------------------------------------------------------------------- //
+
+        // Den Netzwerkstack initialisieren
+        ARP arp = new ARP(config.deviceName, config.ownMacAddress,
+                          config.ownIPAddress, debug)
+
+        arp.start()
+
+        // no need to stop the software, it can be quit with ctrl-c
+
+        sleep(3000)
+
+        println("*** Control: Sending ARP request for $options.t")
+        arp.request(options.t)
+    }
 }
+
+//========================================================================================================//
+// HttpBeispiel-Klasse ENDE
+//========================================================================================================//
+
+
+//========================================================================================================//
+// Hauptprogramm ENDE
+//========================================================================================================//
